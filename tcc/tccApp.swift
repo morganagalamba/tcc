869e4a8f384bd3d2cdf9655416fda630fa632d19
Morgana Galamba <morgana.feijo@gmail.com>
2025-12-29 14:10:08 -0300

    Initial Commit

---
+++ tcc/tccApp.swift
@@ -0,0 +1,17 @@
+//
+//  tccApp.swift
+//  tcc
+//
+//  Created by Morgana Galamba on 29/12/25.
+//
+
+import SwiftUI
+
+@main
+struct tccApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
